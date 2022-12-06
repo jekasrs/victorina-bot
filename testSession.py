5e8f0c7a2532868c56e92b95612bd1a95d8a39a2
Evgeny Smirnov <90210620+jekasrs@users.noreply.github.com>
2022-12-06 19:36:29 +0300

    Update testSession.py

--- testSession.py
+++ testSession.py
@@ -3,14 +3,16 @@ from Session import Session
 #Test cases to test Calulator methods
 #You always create  a child class derived from unittest.TestCase
 class TestSession(unittest.TestCase):
-  #setUp method is overridden from the parent class TestCase
     def setUp(self):
-        self.session = Session(1,2)
-  #Each test method starts with the keyword test_
+        self.session = Session(1,1)
+        self.sessions.append(session)
     def test_admin_id(self):
         self.assertEqual(self.session.get_admin_id(), 1)
     def test_room_id(self):
         self.assertEqual(self.session.get_room_id(), 1)
+    def test_get_next_free_number_of_room(self):
+        self.assertEqual(get_next_free_number_of_room(), 2)
+        
 # Executing the tests in the above test case class
 if __name__ == "__main__":
   unittest.main()
